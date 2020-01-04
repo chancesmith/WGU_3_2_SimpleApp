fc8dbc547d5dedfeb61ad247f6d35644deb400e0
Chance Smith <chancesmithb@gmail.com>
2020-01-04 09:09:33 -0600

    add simple hello world example

---
+++ 3_2_SimpleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _3_2_SimpleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Programming!");
+            Console.WriteLine("test");
+        }
+    }
+}
