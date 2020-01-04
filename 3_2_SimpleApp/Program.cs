ff91d61709c75ed546f4c8637fc486f424bfd8a7
Chance Smith <chancesmithb@gmail.com>
2020-01-04 09:22:12 -0600

    add 3.6 adding ints

--- 3_2_SimpleApp/Program.cs
+++ 3_2_SimpleApp/Program.cs
@@ -2,12 +2,31 @@
 
 namespace _3_2_SimpleApp
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C# Programming!");
-            Console.WriteLine("test");
+            string person = "Chance";
+            Console.Write("Welcome to ");
+            Console.WriteLine("C#\n Programming!");
+            Console.WriteLine($"Welcome to C# Programming, {person}!");
+
+            // 3.6 let's add some ints
+            int number1;
+            int number2;
+            int sum;
+
+            Console.Write($"{person}, enter first integer: "); // prompt user
+            // read first number from user
+            number1 = int.Parse(Console.ReadLine());
+
+            Console.Write($"{person}, enter second integer: ");
+            // read second number from user
+            number2 = int.Parse(Console.ReadLine());
+
+            sum = number1 + number2; // add numbers
+
+            Console.WriteLine($"{person}, the sumb is {sum}");
         }
     }
 }
